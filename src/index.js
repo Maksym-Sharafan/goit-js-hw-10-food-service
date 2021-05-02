877c0d6f4df4c227e7aa19f0dbc32b0413ea1613
Maksym-Sharafan <magsimsharafa@gmail.com>
2021-05-02 16:24:51 +0300

    remove README.md

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import templateMenuCard from './templates/menu-template.hbs';
 import dataMenuCard from './js/menu.json';
-import {randerPage} from './js/script-rander-page.js'
-import { changeThemsByClickAndRelodePage } from './js/script-change-thema.js'
+import { randerPage } from './js/script-rander-page.js';
+import { changeThemsByClickAndRelodePage } from './js/script-change-thema.js';
 import './sass/main.scss';
 
 randerPage(dataMenuCard, templateMenuCard);
